e08e5a411eb4e67beaf3cf534dfc5eab0bdb0018
GhaidaAmour <Ghaida.amour@gmail.com>
2024-05-04 13:22:09 +0300

    create new roles

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 5000;
 
 // Import Routers
-const userRouter = require("./routes/users");
+// const userRouter = require("./routes/users");
 const clinkRouter=require("./routes/clinic")
 const rolesRouter = require("./routes/roles");
 
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 // Routes Middleware
-app.use("/users",userRouter)
+// app.use("/users",userRouter)
 app.use("/clinic",clinkRouter)
 app.use("/roles",rolesRouter)
 
